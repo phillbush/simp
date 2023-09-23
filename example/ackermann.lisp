00b24e7b783716ba39dd20fb98c53cd06da41597
Lucas de Sena <lucas@seninha.org>
2023-09-23 14:57:52 -0300

    ackermann

--- example/ackermann.lisp
+++ example/ackermann.lisp
@@ -1,14 +1,16 @@
-(define ackermann
-  (lambda x y
-    (do
-      (display "compute")
-      (newline)
-      (if (= y 0) 0
-          (= x 0) (* 2 y)
-          (= y 1) 2
-          (ackermann
-            (- x 1)
-            (ackermann x (- y 1)))))))
+(defun ackermann x y
+  (do
+    (display "compute")
+    (newline)
+    (if (= y 0) 0
+        (= x 0) (* 2 y)
+        (= y 1) 2
+        (ackermann
+          (- x 1)
+          (ackermann x (- y 1))))))
+
+(define ack-of-one
+  (ackermann 1))
 
 (display
-  (apply ackermann \(1 6)))
+  (ack-of-one 6))
